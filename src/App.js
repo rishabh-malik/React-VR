ac040370d340ed651c83ea84351af8b1762228e5
rishabh-malik <rishabhmalik249@gmail.com>
2017-12-30 23:17:56 +0530

    adding models

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import {
-  asset,
-  Pano,
-  Text,
-  View,
-} from 'react-vr';
+import { asset, Pano, Text, View, AmbientLight } from 'react-vr';
+import Penholder from './Penholder';
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('background.jpeg')}/>
+        <AmbientLight intensity={ 2.5 } />
+        <Pano source={asset('background.jpeg')} />
         <Text
           style={{
+            backgroundColor: '#777879',
             fontSize: 0.8,
             fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
             paddingLeft: 0.2,
             paddingRight: 0.2,
             textAlign: 'center',
             textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
+            transform: [{translate: [0, 2, -6]}],
           }}>
-         VR-Office
+          My office
         </Text>
+        <Penholder />
       </View>
     );
   }
