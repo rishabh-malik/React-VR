05a952143ed06c006e83c5535e0b0b4ee26a30b2
rishabh-malik <rishabhmalik249@gmail.com>
2017-12-30 22:44:25 +0530

    adding 360 background

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  asset,
+  Pano,
+  Text,
+  View,
+} from 'react-vr';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Pano source={asset('background.jpeg')}/>
+        <Text
+          style={{
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -3]}],
+          }}>
+         VR-Office
+        </Text>
+      </View>
+    );
+  }
+};
